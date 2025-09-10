8f751d1151d9344e2afb394b9cadc596d2a1cfdb
Your Name <you@example.com>
2025-09-10 23:20:01 +0530

    initial pluging commit

---
+++ fakestore-sync.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Sync FakeStore 
+ * Description: Sync products from FakeStoreAPI
+ * Version:     1.0.0
+ * Author:      kasun kalya
+ */
+
+namespace SyncFakeStore;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
