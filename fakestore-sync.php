14141927ac2cf2505ad2927dd1570ee69c74e87e
Your Name <you@example.com>
2025-09-10 23:21:54 +0530

    initial pluging commit

--- fakestore-sync.php
+++ fakestore-sync.php
@@ -1,13 +1,25 @@
 <?php
 /**
- * Plugin Name: Sync FakeStore 
+ * Plugin Name:  FakeStore Sync
  * Description: Sync products from FakeStoreAPI
- * Version:     1.0.0
+ * Version:     1.0
  * Author:      kasun kalya
  */
 
-namespace SyncFakeStore;
+namespace FakeStoreSync;
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+define( 'FAKESTORE_SYNC_PATH', plugin_dir_path( __FILE__ ) );
+define( 'FAKESTORE_SYNC_URL',  plugin_dir_url( __FILE__ ) );
+define( 'FAKESTORE_SYNC_VER',  '1.0' );
+
+require_once FAKESTORE_SYNC_PATH . 'includes/class-plugin.php';
+
+add_action( 'plugins_loaded', function() {
+    \FakeStoreSync\Plugin::init();
+});
